bbaf3b44b4e1764a156afa8c07af7aae0cdeb6ef
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-02-03 23:35:00 -0300

    C0 - Creating Start Project - Back and Frontend

---
+++ Backend/src/test/java/com/example/salesdashboard/SalesdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
