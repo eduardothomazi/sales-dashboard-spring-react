abfc73ccfc9024b665fc752c3afedd50dd9b14e0
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-02-05 09:00:24 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <div >
       <h1 className="text-primary">Hello world!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
